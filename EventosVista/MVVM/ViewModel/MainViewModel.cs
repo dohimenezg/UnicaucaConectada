34e5a565e20bc408aa8db1416bd564a8d446e6eb
jandresordonez <jandresordonez@unicauca.edu.co>
2022-02-28 14:19:26 -0500

    Rama con la aplicacion de interfaz grafica

---
+++ EventosVista/MVVM/ViewModel/MainViewModel.cs
@@ -0,0 +1,37 @@
+﻿using EventosVista.MVVM.Model;
+using System.Collections.ObjectModel;
+
+namespace EventosVista.MVVM.ViewModel
+{
+    internal class MainViewModel
+    {
+        public ObservableCollection<Item> Songs { get; set; }
+        public MainViewModel()
+        {
+            Songs = new ObservableCollection<Item>();
+            PopulateCollection();
+        }
+
+        void PopulateCollection()
+        {
+            var item = new Item();
+            Item[] Items = new Item[3];
+
+            Items[0] = new Item();
+            Items[0].Id = "1";
+            Items[0].Name = "Test1";
+            Items[1] = new Item();
+            Items[1].Id = "2";
+            Items[1].Name = "Test2";
+            Items[2] = new Item();
+            Items[2].Id = "3";
+            Items[2].Name = "Test3";
+
+            for (int i = 0; i < 3; i++)
+            {
+                var track = Items[i];
+                Songs.Add(track);
+            }
+        }
+    }
+}
