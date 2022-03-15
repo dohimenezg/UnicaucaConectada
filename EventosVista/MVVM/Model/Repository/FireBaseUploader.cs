fc93ebf0159bdd2e043f35ab5639f752853424bb
pdaniel102 <pdaniel@unicauca.edu.co>
2022-03-14 23:14:28 -0500

    Firebase Comments deleted

--- EventosVista/MVVM/Model/Repository/FireBaseUploader.cs
+++ EventosVista/MVVM/Model/Repository/FireBaseUploader.cs
@@ -16,23 +16,20 @@ namespace EventosVista.MVVM.Model.Repository
             try
             {
                 var stream = File.OpenRead(pathToFile);
-                //var auth = new FirebaseAuthProvider(new FirebaseConfig("AIzaSyDj4h0NvWvATAjBxvIpazVqWPf-gtS9a0g"));
-                //var a = await auth.SignInWithEmailAndPasswordAsync("pdaniel@unicauca.edu.co", "user123");
+               
                 var task = new FirebaseStorage(
                 "unicaucaconectada-images.appspot.com",
                 new FirebaseStorageOptions
                 {
-                //AuthTokenAsyncFactory = () => Task.FromResult(a.FirebaseToken),
+                
                 ThrowOnCancel = true,
                 })
                 .Child("images")
                 .Child(Path.GetFileName(pathToFile))
                 .PutAsync(stream);
 
-                // Track progress of the upload
                 task.Progress.ProgressChanged += (s, e) => Console.WriteLine($"Progress: {e.Percentage} %");
 
-                // await the task to wait until upload completes and get the download url
                 var downloadUrl = await task;
 
                 latestUpload = downloadUrl.Substring(0, downloadUrl.IndexOf("&"));
